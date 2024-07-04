b33f4dd251bdc427c5f32581fac4b44195fbe4dc
pc <ak5585060@gmail.com>
2024-07-04 13:26:34 +0300

    create note cubit and create fetch method

--- lib/cubits/notes_cubit/notes_cubit_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
+import 'package:project_local_storage/core/constant.dart';
 import 'package:project_local_storage/model/note_model.dart';
 
 part 'notes_cubit_state.dart';
 
-class NotesCubitCubit extends Cubit<NotesCubitState> {
-  NotesCubitCubit() : super(NotesCubitInitial());
+class NotesCubit extends Cubit<NotesCubitState> {
+  NotesCubit() : super(NotesCubitInitial());
+
+  fetchAllNotes() {
+    try {
+      var notebox = Hive.box<NoteModel>(kNotesBox);
+      var note = notebox.values.toList();
+      emit(NotesCubitSuccess(notes: note));
+    } catch (e) {
+      emit(NotesCubitFailure(errMessage: e.toString()));
+    }
+  }
 }
--- lib/view/screens/notes_view.dart
+++ lib/view/screens/notes_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:project_local_storage/cubits/notes_cubit/notes_cubit_cubit.dart';
 import 'package:project_local_storage/view/widgets/add_note_bottom_sheet.dart';
 import 'package:project_local_storage/view/widgets/notes_view_body.dart';
 
@@ -7,26 +9,29 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.blueAccent,
-        shape: const CircleBorder(
-            side: BorderSide(width: 0, color: Colors.blueAccent)),
-        onPressed: () {
-          showBottomSheet(context);
-        },
-        child: const Icon(
-          Icons.add,
-          color: Colors.black,
-          size: 25,
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.blueAccent,
+          shape: const CircleBorder(
+              side: BorderSide(width: 0, color: Colors.blueAccent)),
+          onPressed: () {
+            showBottomSheet(context);
+          },
+          child: const Icon(
+            Icons.add,
+            color: Colors.black,
+            size: 25,
+          ),
         ),
-      ),
-      body: const Padding(
-        padding: EdgeInsets.symmetric(
-          horizontal: 24,
-          vertical: 24,
+        body: const Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 24,
+            vertical: 24,
+          ),
+          child: NotesViewBody(),
         ),
-        child: NotesViewBody(),
       ),
     );
   }
