f09dd86dd44e354d6f23b73574c1b474e1e05a84
pc <ak5585060@gmail.com>
2024-07-15 00:18:42 +0300

    make animation for delete icon

--- lib/view/widgets/notes_item.dart
+++ lib/view/widgets/notes_item.dart
@@ -5,73 +5,94 @@ import 'package:project_local_storage/cubits/notes_cubit/notes_cubit_cubit.dart'
 import 'package:project_local_storage/model/note_model.dart';
 import 'package:project_local_storage/view/widgets/edit_note_view.dart';
 
-class NoteItem extends StatelessWidget {
+class NoteItem extends StatefulWidget {
   const NoteItem({super.key, required this.note});
 
   final NoteModel note;
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _NoteItemState createState() => _NoteItemState();
+}
+
+class _NoteItemState extends State<NoteItem> {
+  bool _isDeleted = false;
+
+  void _deleteNote() {
+    setState(() {
+      _isDeleted = true;
+    });
+
+    Future.delayed(const Duration(milliseconds: 300), () {
+      widget.note.delete();
+      ToastFlutter.flutterToast();
+      BlocProvider.of<NotesCubit>(context).fetchAllNotes();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) {
-            return EditNoteView(
-              note: note,
-            );
-          }),
-        );
-      },
-      child: Container(
-        decoration: BoxDecoration(
-          color: Color(note.color),
-          borderRadius: BorderRadius.circular(16),
-        ),
-        padding: const EdgeInsets.only(left: 16, top: 24, bottom: 24),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            ListTile(
-              title: Text(
-                note.title,
-                style: const TextStyle(
-                  fontSize: 26,
-                  color: Colors.black,
+    return AnimatedOpacity(
+      duration: const Duration(milliseconds: 300),
+      opacity: _isDeleted ? 0.0 : 1.0,
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) {
+              return EditNoteView(
+                note: widget.note,
+              );
+            }),
+          );
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            color: Color(widget.note.color),
+            borderRadius: BorderRadius.circular(16),
+          ),
+          padding: const EdgeInsets.only(left: 16, top: 24, bottom: 24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              ListTile(
+                title: Text(
+                  widget.note.title,
+                  style: const TextStyle(
+                    fontSize: 26,
+                    color: Colors.black,
+                  ),
+                ),
+                subtitle: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  child: Text(
+                    widget.note.subTitle,
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black.withOpacity(.4),
+                    ),
+                  ),
+                ),
+                trailing: IconButton(
+                  onPressed: _deleteNote,
+                  icon: const Icon(
+                    Icons.delete,
+                    color: Colors.black,
+                    size: 30,
+                  ),
                 ),
               ),
-              subtitle: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
                 child: Text(
-                  note.subTitle,
+                  widget.note.date,
                   style: TextStyle(
-                    fontSize: 18,
                     color: Colors.black.withOpacity(.4),
                   ),
                 ),
               ),
-              trailing: IconButton(
-                onPressed: () {
-                  note.delete();
-                  ToastFlutter.flutterToast();
-                  BlocProvider.of<NotesCubit>(context).fetchAllNotes();
-                },
-                icon: const Icon(
-                  Icons.delete,
-                  color: Colors.black,
-                  size: 30,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24),
-              child: Text(
-                note.date,
-                style: TextStyle(
-                  color: Colors.black.withOpacity(.4),
-                ),
-              ),
-            )
-          ],
+            ],
+          ),
         ),
       ),
     );
